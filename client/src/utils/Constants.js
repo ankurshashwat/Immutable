b89b29ac8e072e02a284ffbf68e6a71295645068
ankurshashwat <ankurshshwat@gmail.com>
2023-01-09 01:00:09 +0530

    deployed contract

--- client/src/utils/Constants.js
+++ client/src/utils/Constants.js
@@ -1,6 +1,6 @@
-// Immutable deployed to: 0xB8b167fBC7C963E8b39A5da87a8e7790796507A3
+// Immutable deployed to: 0x59C34D011feC17f9C3DaE64CAE1aDc4d79C342A2
 
 import abi from "./Immutable.json";
 
-export const contractAddress = "0xB8b167fBC7C963E8b39A5da87a8e7790796507A3";
+export const contractAddress = "0x59C34D011feC17f9C3DaE64CAE1aDc4d79C342A2";
 export const contractABI = abi.abi;
